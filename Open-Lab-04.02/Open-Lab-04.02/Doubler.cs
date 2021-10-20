bad11e50b0ee279a6c178f5c87be6070b379fb5c
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-10-20 21:25:49 +0200

    not finished

---
+++ Open-Lab-04.02/Open-Lab-04.02/Doubler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Open_Lab_04._02
+{
+    public class Doubler
+    {
+        char[] arr = new char[20];
+        public string DoubleChar(string original)
+        {
+            int p = original.Length;
+            int i = 0;
+            original.ToCharArray();
+            
+            do
+            {
+                char c = original[i];
+                arr[i] = (c);
+                i++;
+                char b = original[i];
+                arr[i] = (b);
+                i++;
+
+            } while (i < p);
+            string final = arr.ToString();
+            return final;
+
+        }
+    }
+}
